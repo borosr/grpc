51b988104b1666ce20dab07cfc99ad1aebf5e335
Boros Robert <robbie13.boros@gmail.com>
2019-03-25 23:57:48 +0100

    adding syslog plugin to fluentd, created a tcp listener for testing, tested with fluent-go, syslog.Dial working, fluent.conf needs a valid regex for a better parse

--- api/main.go
+++ api/main.go
@@ -19,21 +19,42 @@ const (
 	address1 = "localhost:8086"
 	address2 = "localhost:8087"
 	apiPort  = ":3000"
+	tagname  = "api_log"
+	// fluentd  = "127.0.0.1:24224"
+	fluentd = "127.0.0.1:5140"
+	localD  = "localhost:3333"
 )
 
 var storedCount = int32(0)
 
-func init() {
+func main() {
+	// syslog.New(syslog.LOG_NOTICE, "api_log")
 
-	if logwriter, e := syslog.Dial("udp", "localhost:24224", syslog.LOG_NOTICE, "api_log"); e == nil {
+	// if logwriter, e := syslog.Dial("tcp", localD, syslog.LOG_NOTICE, "api_log"); e == nil {
+	if logwriter, e := syslog.Dial("udp", fluentd, syslog.LOG_NOTICE, "api_log"); e == nil {
 		log.SetOutput(logwriter)
 	} else {
+		defer logwriter.Close()
 		log.Fatal("Syslog init error!")
 	}
-}
 
-func main() {
-	log.Println("API started")
+	// logger, err := fluent.New(fluent.Config{})
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// defer logger.Close()
+
+	// data := map[string]string{
+	// 	"foo":  "bar",
+	// 	"hoge": "hoge",
+	// }
+
+	// if err := logger.Post(tagname, data); err != nil {
+	// panic(err)
+	// }
+
+	log.Print("Hello log")
+
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
